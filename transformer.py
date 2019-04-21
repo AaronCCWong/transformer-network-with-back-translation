ca82986d9f865ec8ae61429bb3cf2903fedcb916
AaronCCWong <aarn.wong@gmail.com>
2019-04-21 03:16:45 -0400

    Add positional encoder

--- transformer.py
+++ transformer.py
@@ -1,19 +1,27 @@
+import math
 import torch.nn as nn
 from decoder import Decoder
 from encoder import Encoder
+from positional_encoder import PositionalEncoder
 
 
 class Transformer(nn.Module):
     def __init__(self, src_vocab_size, tgt_vocab_size, d_model=512):
         super(Transformer, self).__init__()
+        self.d_model = d_model
+
         self.src_embedding = nn.Embedding(src_vocab_size, d_model)
         self.tgt_embedding = nn.Embedding(tgt_vocab_size, d_model)
+        self.positional_encoder = PositionalEncoder(d_model)
         self.encoder = Encoder(6)
         self.decoder = Decoder(6)
         self.linear = nn.Linear(d_model, d_model)
         self.softmax = nn.Softmax()
 
     def forward(self, src, tgt, src_mask, tgt_mask):
-        out = self.src_embedding(src)
+        out = self.src_embedding(src) * math.sqrt(self.d_model)
+        out = self.positional_encoder(out)
         encoded_input = self.encoder(out, src_mask)
-        return self.decoder(src, encoded_input, src_mask, tgt, tgt_mask)
+        out = self.tgt_embedding(tgt) * math.sqrt(self.d_model)
+        out = self.positional_encoder(out)
+        return self.decoder(out, encoded_input, tgt_mask)
